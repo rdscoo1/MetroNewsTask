d52494801ec2f07f0d77a38edee63d59ed841d5a
Roman <romakhodukin@gmail.com>
2021-04-29 18:48:04 +0300

    Refactoring Error.xib, DatasourceDelegate object

--- MetroNewsTask/Modules/News/Controller/NewsTableViewDataSourceDelegate.swift
+++ MetroNewsTask/Modules/News/Controller/NewsTableViewDataSourceDelegate.swift
@@ -25,25 +25,22 @@ class NewsTableViewDataSourceDelegate: NSObject {
 // MARK: - UITableViewDataSource
 
 extension NewsTableViewDataSourceDelegate: UITableViewDataSource {
-
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        if case .loaded(let data) = props {
+        switch props {
+        case .loaded(let data):
             return data.count
-        } else if case .loading = props{
-            return 1
-        } else if case .error = props {
+        case .loading, .error:
             return 1
         }
-        
-        return 0
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        if case .loaded(let data) = props {
+        switch props {
+        case .loaded(let data):
             guard let tweet = data[safe: indexPath.row] else {
                 return UITableViewCell()
             }
-            
             if !tweet.mediaEntities.isEmpty {
                 guard let tweetCell = tableView.dequeueReusableCell(withIdentifier: TweetTableViewCell.reuseId, for: indexPath) as? TweetTableViewCell
                 else { return UITableViewCell() }
@@ -59,22 +56,20 @@ extension NewsTableViewDataSourceDelegate: UITableViewDataSource {
                 
                 return tweetWithoutImageCell
             }
-        } else if case .loading = props {
-            let loadingCell = tableView.dequeueReusableCell(withIdentifier: "LoadingTableViewCell", for: indexPath)
-                        
-            return loadingCell
-        } else if case .error(let errorData) = props {
+        case .error(let errorData):
             guard let errorCell = tableView.dequeueReusableCell(withIdentifier: ErrorTableViewCell.reuseId, for: indexPath) as? ErrorTableViewCell
             else { return UITableViewCell() }
             
-            errorCell.didTapTryAgain = {                
+            errorCell.didTapTryAgain = {
                 errorData.action()
             }
             
             return errorCell
+        case .loading:
+            let loadingCell = tableView.dequeueReusableCell(withIdentifier: "LoadingTableViewCell", for: indexPath)
+            
+            return loadingCell
         }
-        
-        return UITableViewCell()
     }
 }
 
@@ -84,7 +79,7 @@ extension NewsTableViewDataSourceDelegate: UITableViewDelegate {
     
     func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
         cell.alpha = 0
-
+        
         UIView.animate(withDuration: 0.3, delay: 0, animations: {
             cell.alpha = 1
         })
